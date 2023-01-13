7c626ac33cd1ad767ed8889f633789f55810ad68
Chloe Nielsen <littleb88888888@gmail.com>
2023-01-13 12:55:43 -0700

    Added basic title screen layout
    - UI Text Populator created to simplify creation or editing or of team and sponsor lists

---
+++ Assets/Scripts/Menus/UITextPopulator.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Sirenix.Utilities;
+
+public class UITextPopulator : MonoBehaviour
+{
+    public string[] memberStrings;
+    public GameObject prototype;
+    public bool usePrototypeInstance = true;
+
+    void Start()
+    {
+        PopulateTextList();
+    }
+
+    void PopulateTextList()
+    {
+        int memberIndex = 0;
+        if (usePrototypeInstance)
+        {
+            prototype.GetComponent<TextMeshProUGUI>().text = memberStrings[memberIndex];
+            memberIndex++;
+        }
+
+        while(memberIndex < memberStrings.Length)
+        {
+            GameObject prototypeClone = Instantiate(prototype);
+            SetProtoypeInstanceValues(prototypeClone, memberStrings[memberIndex]);
+
+            memberIndex++;
+        }
+    }
+
+    void SetProtoypeInstanceValues(GameObject prototypeInstance, string memberString)
+    {
+        prototypeInstance.transform.SetParent(transform, false);
+        prototypeInstance.name = memberString;
+        prototypeInstance.GetComponent<TextMeshProUGUI>().text = memberString;
+    }
+}
