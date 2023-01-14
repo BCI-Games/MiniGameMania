34b86132264f259bad368261d78c9494060d9923
Chloe Nielsen <littleb88888888@gmail.com>
2023-01-13 19:44:44 -0700

    Switched transitions to use large arrow wipe

--- Assets/Scripts/Menus/MenuStateBehavior.cs
+++ Assets/Scripts/Menus/MenuStateBehavior.cs
@@ -4,18 +4,22 @@ using UnityEngine.UI;
 public class MenuStateBehavior : MonoBehaviour
 {
     public GameObject currentMenu;
+    public RectTransform transitionShape;
     public GameObject transitionRaycastBlocker;
 
     public float transitionPeriod;
     public AnimationCurve transitionEasing;
     public Vector2 screenSize = new Vector2(450, 800);
     public Vector2 transitionBlockSize = new Vector2(600, 950);
+    public float menuTravelDistance = 200;
 
     SlideTransitionInstance previousMenuTransition;
     SlideTransitionInstance currentMenuTransition;
+    SlideTransitionInstance shapeTransition;
 
     bool transitionActive;
     float transitionTimer;
+    float transitionShapeDiagonal;
 
     void Start()
     {
@@ -24,9 +28,11 @@ public class MenuStateBehavior : MonoBehaviour
         currentMenu.SetActive(true);
 
         transitionRaycastBlocker.SetActive(false);
+
+        transitionShapeDiagonal = transitionShape.sizeDelta.x * 1.42f;
+        transitionShape.gameObject.SetActive(false);
     }
 
-    // Update is called once per frame
     void Update()
     {
         if(transitionActive)
@@ -40,6 +46,13 @@ public class MenuStateBehavior : MonoBehaviour
                 float lerpValue = transitionEasing.Evaluate(transitionTimer / transitionPeriod);
                 previousMenuTransition.SetPosition(lerpValue);
                 currentMenuTransition.SetPosition(lerpValue);
+                shapeTransition.SetPosition(lerpValue);
+
+                if (lerpValue >= 0.3f)
+                {
+                    previousMenuTransition.ResetTarget();
+                    currentMenu.SetActive(true);
+                }
             }
             else
             {
@@ -47,23 +60,38 @@ public class MenuStateBehavior : MonoBehaviour
                 transitionRaycastBlocker.SetActive(false);
 
                 previousMenuTransition.ResetTarget();
+                shapeTransition.ResetTarget();
             }
         }
     }
 
     public void SwitchTo(MenuTransitionSettings transitionSettings)
     {
+        //transitionActive = true;
+        //transitionRaycastBlocker.SetActive(true);
+        //transitionTimer = 0;
+
+        //Vector2 transitionDistance = transitionBlockSize + screenSize;
+        //transitionDistance.x *= transitionSettings.direction.x;
+        //transitionDistance.y *= transitionSettings.direction.y;
+
+        //previousMenuTransition = new(currentMenu, Vector2.zero, transitionDistance);
+        //currentMenu = transitionSettings.targetMenu;
+        //currentMenuTransition = new(currentMenu, -transitionDistance, transitionDistance);
+
         transitionActive = true;
         transitionRaycastBlocker.SetActive(true);
         transitionTimer = 0;
 
-        Vector2 transitionDistance = transitionBlockSize + screenSize;
-        transitionDistance.x *= transitionSettings.direction.x;
-        transitionDistance.y *= transitionSettings.direction.y;
+        Vector2 menuDistance = Vector2.left * menuTravelDistance;
 
-        previousMenuTransition = new(currentMenu, Vector2.zero, transitionDistance);
+        previousMenuTransition = new(currentMenu, Vector2.zero, menuDistance);
         currentMenu = transitionSettings.targetMenu;
-        currentMenuTransition = new(currentMenu, -transitionDistance, transitionDistance);
+        currentMenuTransition = new(currentMenu, -menuDistance, menuDistance);
+        currentMenu.SetActive(false);
+
+        Vector2 shapeDistance = Vector2.left * (transitionShapeDiagonal + screenSize.x / 2);
+        shapeTransition = new(transitionShape.gameObject, -shapeDistance / 2, shapeDistance);
     }
 
     class SlideTransitionInstance
