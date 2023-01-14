8bb9a683c581cfbc3ac3dad6c25fa31d184af405
Chloe Nielsen <littleb88888888@gmail.com>
2023-01-14 12:50:01 -0700

    Update MenuStateBehavior.cs
    - updated script missing from last commit

--- Assets/Scripts/Menus/MenuStateBehavior.cs
+++ Assets/Scripts/Menus/MenuStateBehavior.cs
@@ -4,22 +4,18 @@ using UnityEngine.UI;
 public class MenuStateBehavior : MonoBehaviour
 {
     public GameObject currentMenu;
-    public RectTransform transitionShape;
     public GameObject transitionRaycastBlocker;
 
     public float transitionPeriod;
     public AnimationCurve transitionEasing;
     public Vector2 screenSize = new Vector2(450, 800);
-    public Vector2 transitionBlockSize = new Vector2(600, 950);
-    public float menuTravelDistance = 200;
+    public float transitionOffset = 200;
 
     SlideTransitionInstance previousMenuTransition;
     SlideTransitionInstance currentMenuTransition;
-    SlideTransitionInstance shapeTransition;
 
     bool transitionActive;
     float transitionTimer;
-    float transitionShapeDiagonal;
 
     void Start()
     {
@@ -28,9 +24,6 @@ public class MenuStateBehavior : MonoBehaviour
         currentMenu.SetActive(true);
 
         transitionRaycastBlocker.SetActive(false);
-
-        transitionShapeDiagonal = transitionShape.sizeDelta.x * 1.42f;
-        transitionShape.gameObject.SetActive(false);
     }
 
     void Update()
@@ -46,13 +39,6 @@ public class MenuStateBehavior : MonoBehaviour
                 float lerpValue = transitionEasing.Evaluate(transitionTimer / transitionPeriod);
                 previousMenuTransition.SetPosition(lerpValue);
                 currentMenuTransition.SetPosition(lerpValue);
-                shapeTransition.SetPosition(lerpValue);
-
-                if (lerpValue >= 0.3f)
-                {
-                    previousMenuTransition.ResetTarget();
-                    currentMenu.SetActive(true);
-                }
             }
             else
             {
@@ -60,38 +46,23 @@ public class MenuStateBehavior : MonoBehaviour
                 transitionRaycastBlocker.SetActive(false);
 
                 previousMenuTransition.ResetTarget();
-                shapeTransition.ResetTarget();
             }
         }
     }
 
     public void SwitchTo(MenuTransitionSettings transitionSettings)
     {
-        //transitionActive = true;
-        //transitionRaycastBlocker.SetActive(true);
-        //transitionTimer = 0;
-
-        //Vector2 transitionDistance = transitionBlockSize + screenSize;
-        //transitionDistance.x *= transitionSettings.direction.x;
-        //transitionDistance.y *= transitionSettings.direction.y;
-
-        //previousMenuTransition = new(currentMenu, Vector2.zero, transitionDistance);
-        //currentMenu = transitionSettings.targetMenu;
-        //currentMenuTransition = new(currentMenu, -transitionDistance, transitionDistance);
-
         transitionActive = true;
         transitionRaycastBlocker.SetActive(true);
         transitionTimer = 0;
 
-        Vector2 menuDistance = Vector2.left * menuTravelDistance;
+        Vector2 transitionDistance = Vector2.one * transitionOffset + screenSize;
+        transitionDistance.x *= transitionSettings.direction.x;
+        transitionDistance.y *= transitionSettings.direction.y;
 
-        previousMenuTransition = new(currentMenu, Vector2.zero, menuDistance);
+        previousMenuTransition = new(currentMenu, Vector2.zero, transitionDistance);
         currentMenu = transitionSettings.targetMenu;
-        currentMenuTransition = new(currentMenu, -menuDistance, menuDistance);
-        currentMenu.SetActive(false);
-
-        Vector2 shapeDistance = Vector2.left * (transitionShapeDiagonal + screenSize.x / 2);
-        shapeTransition = new(transitionShape.gameObject, -shapeDistance / 2, shapeDistance);
+        currentMenuTransition = new(currentMenu, -transitionDistance, transitionDistance);
     }
 
     class SlideTransitionInstance
