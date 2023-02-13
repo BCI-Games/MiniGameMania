ce476a7d146501157ffc0efc85eb30e833fba27c
Chloe Nielsen <littleb88888888@gmail.com>
2023-02-13 16:24:56 -0700

    Updated training scene
    Updated basic p300 training scene to make it more engaging without adding significant complexity

---
+++ Assets/Scripts/Menus/Training/SignalSPOFactory.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using System;
+
+public class SignalSPOFactory : MonoBehaviour
+{
+    public GameObject prototype;
+    public int count;
+
+    public UnityEvent<SPO> OnSelection;
+    public UnityEvent<SPO> OnTrainTarget;
+    public UnityEvent<SPO> OnStimulus;
+
+    List<SignalSPO> childSPOs;
+
+    void Start()
+    {
+        childSPOs = new();
+
+        for(int i = 0; i < count; i++)
+        {
+            GameObject spoInstance = prototype;
+            
+            if(i != 0)
+            {
+                spoInstance = Instantiate(prototype);
+                spoInstance.transform.SetParent(transform, false);
+            }
+
+            SignalSPO spo = spoInstance.GetComponentInChildren<SignalSPO>();
+            spo.myIndex = i;
+            spo.SetEvents(OnSelection.Invoke, OnTrainTarget.Invoke, OnStimulus.Invoke);
+
+            childSPOs.Add(spo);
+        }
+    }
+
+    public void DisableChildrenTargetIndicators()
+    {
+        foreach (SignalSPO spo in childSPOs)
+            spo.DisableTargetIndicator();
+    }
+}
